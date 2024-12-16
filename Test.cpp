d803e440ac29acb86e41b4be82b630393c7dcb1f
LuckyCoderName <danila.seleznev.03@gmail.com>
2024-12-17 00:25:33 +0400

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,17 +1,16 @@
-﻿#include "stdio.h"
-
+#include <iostream>
 void First() { // самое длинное слово
 	int max = 0;
 	int bigWordStart = 0;
 	char str[100] = " worldddd woord lol h";
-	for (int i=0,j = 0; str[i] != '\0'; i++, j++) {
-		if ((str[i] == ' ')|| (str[i+1] == '\0')) {
+	for (int i = 0, j = 0; str[i] != '\0'; i++, j++) {
+		if ((str[i] == ' ') || (str[i + 1] == '\0')) {
 			if (str[i + 1] == '\0') {
 				j++;
 				i++;
 			}
 			if (max < j) {
-				
+
 				max = j;
 				bigWordStart = i - j;
 			}
@@ -26,7 +25,7 @@ void First() { // самое длинное слово
 
 
 void Second() { //  удалить двойные цифры
-	char str[100] = "111 23 22 1313 1 1";
+	char str[100] = "22 1154 22 123 22 2 13 1";
 
 	for (int i = 0, j = 0; str[i] != '\0'; i++, j++) {
 		if ((str[i] == ' ') || (str[i + 1] == '\0')) {
@@ -34,51 +33,84 @@ void Second() { //  удалить двойные цифры
 				j++;
 				i++;
 			}
-			if (j == 3) {
-				str[i-1] = ' ';
-				str[i-2] = ' ';
+			if (j == 2) {
+				/*str[i - 1] = ' ';
+				str[i - 2] = ' ';*/
+				for (int k = i-2; str[k]!='\0'; k++)
+				{
+					str[k] = str[k+3];
+				}
+				i -= 2;
+				j = 0;
 			}
-			j = 0;
+			
+			if (j != 0) 
+			{
+				j = -1;
+			}
+
 		}
 
 	}
 	puts(str);
 }
 
+int StrLen(char* str) { //длинна строки
+	int len = 0;
+	for (; str[len] != '\0'; len++);
+	return len;
+}
+
 void Third() {
 	int isPal = 0;
-	char str[100] = " assda aaaaa dadd dss sds"; // добавить |
-	
+	char str[100] = "aaa DSW adda WSAD a"; // добавить |
+	//printf("%i", len);
 
 	for (int i = 0, j = 0; str[i] != '\0'; i++, j++)
 	{
 		if (((str[i] == ' ')) || (str[i + 1] == '\0'))
 		{
-			if ((i==0) && (str[i]==' '))
+			if ((i == 0) && (str[i] == ' '))
 			{
 				j = -1;
 				continue;
 			}
-			if (str[i + 1] == '\0') 
+			if (str[i + 1] == '\0')
 			{
 				j++;
 				i++;
 			}
-			//printf("j = %i ", j);
 
 			isPal = 1;
 
 			for (int temp = 0; temp < j; temp++)
 			{
-				//printf("str = %c %c\n", str[i - 1 - temp], str[i + temp-j]);
-				if (str[i - 1 - temp] != str[i + temp - j]) 
+				if (str[i - 1 - temp] != str[i + temp - j])
 				{
 					isPal = 0;
 				}
 			}
 			if (isPal == 1) {
-				str[i - j - 1] = '|';
-				str[i] = '|';
+				//printf("%i", StrLen(str));
+				for (int k = StrLen(str)+2; k!=-1; k--)
+				{
+					
+					if (k >i+1)
+					{
+						str[k] = str[k - 2]; 
+					}
+					else if (k == i + 1) {
+						str[k] = '|';
+					}
+					else if((k < i + 1)&&(k>i-j)) {
+						str[k] = str[k - 1];
+					}
+					else if (k == i - j){
+						str[k] = '|';
+					}
+				}
+				/*puts(str);*/
+				i += 2;
 			}
 			j = -1;
 			//printf("isPal = %i ", isPal);
@@ -92,20 +124,21 @@ void Four() {// наибольшая сумма эл матриц
 	int indexj = 0;
 	int podmatrizaSize;
 	int sumMatr = 0;
-	int maxSum=0;
-	int matr[4][5] = {{1,2,3,4,5},
+	int maxSum = 0;
+	int matr[4][5] = { {1,2,3,4,5},
 					{5,5,3,4,5},
 					{5,5,3,4,5},
-					{1,2,3,4,5},};
+					{1,2,3,4,5}, };
+	printf("Enter the size of the submatrix: ");
 	scanf_s("%i", &podmatrizaSize);
-	for (int i = 0; i < 4 - podmatrizaSize+1; i++)
+	for (int i = 0; i < 4 - podmatrizaSize + 1; i++)
 	{
-		for (int j = 0; j < 5 - podmatrizaSize+1; j++)
+		for (int j = 0; j < 5 - podmatrizaSize + 1; j++)
 		{
 			sumMatr = 0;
-			for (int i1 = i; i1 < podmatrizaSize+i; i1++)
+			for (int i1 = i; i1 < podmatrizaSize + i; i1++)
 			{
-				for (int j1 = j; j1 < podmatrizaSize+j; j1++)
+				for (int j1 = j; j1 < podmatrizaSize + j; j1++)
 				{
 					sumMatr += matr[i1][j1];
 					printf("%i ", matr[i1][j1]);
@@ -122,7 +155,7 @@ void Four() {// наибольшая сумма эл матриц
 	}
 	printf("best matr\n");
 	for (int i = indexi; i < indexi + podmatrizaSize; i++) {
-		for (int j = indexj; j < indexj+podmatrizaSize; j++)
+		for (int j = indexj; j < indexj + podmatrizaSize; j++)
 		{
 			printf("%i ", matr[i][j]);
 		}
@@ -134,6 +167,6 @@ void Four() {// наибольшая сумма эл матриц
 int main() {
 	//First();
 	//Second();
-	Third();
-	//Four();
+	//Third();
+	Four();
 }
